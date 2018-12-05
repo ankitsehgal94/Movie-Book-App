19e3502f9ee7891329d69300300d0f59e6926360
ankitsehgal94 <ankit.kumar89.ak57@gmail.com>
2018-12-05 12:40:44 +0530

    First changes

---
+++ index.js
@@ -0,0 +1,13 @@
+/** @format 
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent('MovieTickets', () => App);
+*/
+
+import { AppRegistry } from 'react-native';
+import App from './src/app';
+
+AppRegistry.registerComponent('MovieTickets', () => App);
